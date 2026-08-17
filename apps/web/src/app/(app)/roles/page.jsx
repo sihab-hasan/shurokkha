23d7d8906c7eb8782a7764a773ca534681443b15
abidhasan176 <abidprodhan176@gmail.com>
2026-08-18 02:37:34 +0600

    style: format code with prettier

--- apps/web/src/app/(app)/roles/page.jsx
+++ apps/web/src/app/(app)/roles/page.jsx
@@ -1,11 +1,11 @@
-'use client';
+"use client"
 
-import CreateRole from '@/components/CreateRole';
+import CreateRole from "@/components/CreateRole"
 
 export default function RolesPage() {
   return (
     <div className="container mx-auto p-4">
       <CreateRole />
     </div>
-  );
+  )
 }
